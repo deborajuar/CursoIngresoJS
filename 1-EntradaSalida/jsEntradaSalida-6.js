198bc9f6f01ecf8231a6bda5e47b559f6f72a87f
Debora <40776559+deborajuar@users.noreply.github.com>
2018-07-05 20:42:32 -0300

    6, 8 y 9

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -4,17 +4,32 @@ mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
 
-	var numerouno;
-	var numerodos;
+	/*var numeroUno;
+	var numeroDos;
 	var resultado;
-	numerouno=document.getElementById('numeroUno').value;
-	numerodos=document.getElementById('numeroDos').value;
-	parseInt(numerouno);
-	parseInt(numerodos);
-	resultado=parseInt(numerouno)+parseInt(numerodos);
-	console.log("La suma es "+resultado);
-	alert("La suma es "+resultado);
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroDos=document.getElementById('numeroDos').value;
+	parseInt(numeroUno);
+	parseInt(numeroDos);
+	resultado=parseInt(numeroUno)+parseInt(numeroDos);
+	alert("La suma es "+resultado);*/
 
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+	resultado=numeroUno+numeroDos;
+	alert(resultado);
+
+/*el signo + es java sirve para dos cosas, para sumar para concatenar.
+se puede pasar a un numero de dos formas 1. TRANSFORMAR VALOR 2. esta funcion lo que hace es "devolver",
+ no transformar como la anterior... esta función es la paseInt. 
+
+*/
 
 }
 	
