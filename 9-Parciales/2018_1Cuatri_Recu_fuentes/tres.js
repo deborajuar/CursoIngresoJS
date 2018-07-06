fe0a1c2e2e613035feaca5cae65244c1aa558e4f
Debora <40776559+deborajuar@users.noreply.github.com>
2018-07-06 02:24:03 -0300

    3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var descuentoDinero;
+	var precioDescuento;
+	var iva;
+	var precioFinal;
+
+	precio=prompt("Precio");
+	precio=parseInt(precio);
+	porcentaje=prompt("porcentaje");
+	porcentaje=parseInt(porcentaje);
+	descuentoDinero=(porcentaje*precio)/100;
+	descuentoDinero=parseInt(descuentoDinero);
+	precioDescuento=precio-descuentoDinero;
+	iva=(precioDescuento*21)/100;
+	iva=parseInt(iva);
+	precioFinal=parseInt(precioFinal);
+	precioFinal=precioDescuento+iva;
+	document.getElementById('elPrecioFinal').value=precioFinal;
+
+	alert("El descuento es de $"+descuentoDinero+". El precio con el descuento es $"+precioDescuento);
+
+
 
 }
