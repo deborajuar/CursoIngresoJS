8cd1800464bfbca1a690200f5311348364755ad3
Debora <40776559+deborajuar@users.noreply.github.com>
2018-07-06 01:46:41 -0300

    1,  2 y 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var precioFinal;
 
+	precio=prompt("Precio");
+	porcentaje=prompt("Porcentaje");
+	precioFinal=precio-(porcentaje*precio/100);
+	precio=parseInt(precio);
+	porcentaje=parseInt(porcentaje);
+	document.getElementById('elPrecioFinal').value=precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,15 @@
 
 function mostrar()
 {
-
+	var ancho;
+	var largo;
+	var perimetro;
+	
+	ancho=prompt("ancho");
+	largo=prompt("largo");
+	ancho=parseInt(ancho);
+	Largo=parseInt(largo);
+	perimetro=ancho*2+largo*2;
+	
+	alert("El perímetro es "+perimetro);
 }
