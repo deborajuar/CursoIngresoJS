f7f13d83c4aeec02274f83b530db10f976e59150
Debora <40776559+deborajuar@users.noreply.github.com>
2018-07-06 01:47:47 -0300

    1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
+
+	base=prompt("Base");
+	base=parseInt(base);
+	altura=prompt("Altura");
+	altura=parseInt(altura);
+	superficie=(base*altura)/2;
+	perimetro=base*3;
+	alert("El perímetro es "+perimetro+" La superficie es "+superficie);
 
 }
