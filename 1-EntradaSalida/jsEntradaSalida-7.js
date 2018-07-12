c674a60c8a15491d9c1cc42f82318b1dd547c7d4
Debora <40776559+deborajuar@users.noreply.github.com>
2018-07-12 13:46:55 -0300

    E/S7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,69 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
+{
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+	resultado=numeroUno+numeroDos;
+
+	alert("El resultado de la suma es: "+resultado);
+
 	
 }
 
 function restar()
 {
+	var numeroUno; 
+	var numeroDos;
+	var resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+	resultado=numeroUno-numeroDos;
+
+
+		alert("El resultado de la resta es: "+resultado);
+
 	
 }
 
 function multiplicar()
 { 
-	
+	var numeroUno; 
+	var numeroDos;
+	var resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+	resultado=numeroUno*numeroDos;
+
+		alert("El producto es: "+resultado);
+
 }
 
 function dividir()
 {
-	
+	var numeroUno; 
+	var numeroDos;
+	var resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+	resultado=numeroUno/numeroDos;
+
+		alert("El cociente es: "+resultado);
+
 }
 
